9ac57c885055827580ee88a264cc75cfa24e1464
Apolo915 <arthurmatsonp07@hotmail.com>
2023-11-01 01:53:21 -0400

    Chapter 9 - Added Gunshot audio

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,6 +6,10 @@ public class Gun : MonoBehaviour
 {
     public float fireRate;
     protected float lastFireTime;
+    public Ammo ammo;
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +19,18 @@ public class Gun : MonoBehaviour
     protected virtual void Update()
     {
     }
+
     protected void Fire()
     {
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
         GetComponentInChildren<Animator>().Play("Fire");
     }
 }
