3e098ed4692925705be099192254905be3e5a201
Apolo915 <arthurmatsonp07@hotmail.com>
2023-11-07 02:47:19 -0500

    Chapter 10 - Added animation for robot and added fired robot missiles

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     [SerializeField]
+    GameObject missileprefab;
+    [SerializeField]
     private string robotType;
     public int health;
     public int range;
@@ -14,6 +16,7 @@ public class Robot : MonoBehaviour
     private Transform player;
     private float timeLastFired;
     private bool isDead;
+    public Animator robot;
 
     void Start()
     {
@@ -45,6 +48,9 @@ public class Robot : MonoBehaviour
     }
     private void fire()
     {
-        Debug.Log("Fire");
+        GameObject missile = Instantiate(missileprefab);
+        missile.transform.position = missileFireSpot.transform.position;
+        missile.transform.rotation = missileFireSpot.transform.rotation;
+        robot.Play("Fire");
     }
 }
