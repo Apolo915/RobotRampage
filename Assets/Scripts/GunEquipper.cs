096e8a1f968beab7445db1a27bf9df4da2759975
Apolo915 <arthurmatsonp07@hotmail.com>
2023-11-07 15:21:59 -0500

    Chapter 11 - Halfway Through coding UI

--- Assets/Scripts/GunEquipper.cs
+++ Assets/Scripts/GunEquipper.cs
@@ -6,11 +6,14 @@ public class GunEquipper : MonoBehaviour
 {
     [SerializeField]
     GameUI gameUI;
+    [SerializeField]
+    Ammo ammo;
     public static string activeWeaponType;
     public GameObject pistol;
     public GameObject assaultRifle;
     public GameObject shotgun;
     GameObject activeGun;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,7 @@ public class GunEquipper : MonoBehaviour
         shotgun.SetActive(false);
         weapon.SetActive(true);
         activeGun = weapon;
+        gameUI.SetAmmoText(ammo.GetAmmo(activeGun.tag));
     }
 
     // Update is called once per frame
