8f081f3bb210ca7adc3998ec05ba23129ff53060
Apolo915 <arthurmatsonp07@hotmail.com>
2023-11-07 03:55:25 -0500

    Chapter 10 - Added robot spawn logic and sounds

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     [SerializeField]
+    private AudioClip deathSound;
+    [SerializeField]
+    private AudioClip fireSound;
+    [SerializeField]
+    private AudioClip weakHitSound;
+    [SerializeField]
     GameObject missileprefab;
     [SerializeField]
     private string robotType;
@@ -53,6 +59,7 @@ public class Robot : MonoBehaviour
         missile.transform.position = missileFireSpot.transform.position;
         missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
     }
 
     public void TakeDamage(int amount)
@@ -67,6 +74,11 @@ public class Robot : MonoBehaviour
             isDead = true;
             robot.Play("Die");
             StartCoroutine("DestroyRobot");
+            GetComponent<AudioSource>().PlayOneShot(deathSound);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(weakHitSound);
         }
     }
     // 2
